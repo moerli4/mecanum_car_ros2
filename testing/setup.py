ceba85c62cd1ff9fb8540c26011c61008a3190f5
moerli4 <moritzgeissler04@gmail.com>
2025-11-19 17:06:06 +0100

    move testing folder out of src

--- testing/setup.py
+++ testing/setup.py
@@ -5,7 +5,7 @@ package_name = "testing"
 setup(
     name=package_name,
     version="0.0.0",
-    packages=find_packages(exclude=["test"]),
+    packages=find_packages(),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
@@ -14,12 +14,9 @@ setup(
     zip_safe=True,
     maintainer="Moritz Geissler",
     maintainer_email="moritz.geissler@tum.de",
-    description="Package for testing scripts",
+    description="Package for tests and experiments",
     license="All rights reserved",
     extras_require={
-        "test": [
-            "pytest",
-        ],
     },
     entry_points={
         "console_scripts": [
