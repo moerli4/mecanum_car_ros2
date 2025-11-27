ae52cec0fe192828e0ae71cd888ae5967df5de9c
moerli4 <moritzgeissler04@gmail.com>
2025-11-27 12:50:45 +0100

    add set_all_motors service to motor driver

--- src/drivers/motor_drivers/motor_drivers/motor_driver.py
+++ src/drivers/motor_drivers/motor_drivers/motor_driver.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 
-from interfaces.srv import SetMotor
+from interfaces.srv import SetMotor,SetAllMotors
 from util.Raspbot_Library import Raspbot
 
 
@@ -15,19 +15,34 @@ class MotorDriverNode(Node):
         self.raspbot_ = Raspbot()
         # create service
         self.srv = self.create_service(SetMotor, "set_motor", self.set_motor_callback)
+        self.srv_all = self.create_service(SetAllMotors, "set_all_motors", self.set_all_motors_callback)
         self.get_logger().info(f"MotorDriverNode initiated")
 
     def set_motor_callback(self, request, response):
+        self.get_logger().info(
+            f"[Set Motor] Incoming request {request.id, request.dir, request.speed}"
+        )
         # set motor at given id to given direction and speed
         response.success = self.raspbot_.Ctrl_Car(
             request.id, request.dir, request.speed
         )
-        self.get_logger().info(
-            f"[Set Motor] Incoming request {request.id, request.dir, request.speed}"
-        )
         return response
 
-
+    def set_all_motors_callback(self, request, response):
+        self.get_logger().info(
+            f"[Set Motor] Incoming request {request.dir, request.speed}"
+        )
+        # set motors to given direction and speed
+        response.success = 0
+        for i, (direction, speed) in enumerate(zip(request.dir,request.speed)):
+            success = self.raspbot_.Ctrl_Car(
+                i, direction, speed
+            )
+            if success != 0:
+                response.success = 1
+                break # break if one fails
+        return response
+    
 def main(args=None):
     rclpy.init(args=args)
     node = MotorDriverNode()
