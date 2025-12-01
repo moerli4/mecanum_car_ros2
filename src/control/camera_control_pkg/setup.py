ff9cebc13bd8cb9b1a8ed3f74372ec2b6acc2c18
z465 <zhenglinjin134@gmail.com>
2025-12-01 15:26:52 +0100

    add package for the camera detection scripts

---
+++ src/control/camera_control_pkg/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'camera_control_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mo',
+    maintainer_email='moritzgeissler04@gmail.com',
+    description='Package for basic camera control, such as mediapipe face detection etc',
+    license='All rights reserved',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "face_detection = camera_control_pkg.face_detection:main",
+            "gesture_detection = camera_control_pkg.gesture_detection:main",
+            "pose_detection = camera_control_pkg.pose_detection:main",
+        ],
+    },
+)
